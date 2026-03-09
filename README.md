bf893af48ad9552bb6f32cfc2989c4f92f2d9878
waladir <ludek@hrbkovi.eu>
2026-03-09 20:51:52 +0100

    1.3.9

--- README.md
+++ README.md
@@ -37,6 +37,9 @@ EPG lze pak stáhnout z http://<adresa nebo jméno serveru>:<port (defaultně 80
 Na http://<adresa nebo jméno serveru>:<port (defaultně 8082)>, např. http://127.0.0.1:8082 je možné stiskem tlačítka vynutit načtení kanálů nebo vytvotvoření nové sessiony.
 
 <b><u>Změny</u></b>
+v1.3.9 (9.3.2026)
+- zpracování synchronních i asynchronních volání API
+
 v1.3.8 (9.3.2026)
 - změna volání API Oneplay
 
@@ -49,6 +52,3 @@ v1.3.6 (6.12.2025)
 v1.3.5 (5.12.2025)
 - oprava přehrávání multidimenze
 - možnost nastavit profilový pin
-
-v1.3.4 (4.12.2025)
-- zkrácení platnosti session na 4 hodiny
--- resources/lib/api.py
+++ resources/lib/api.py
@@ -33,23 +33,45 @@ def call_api(url, data, token = None):
             data = response.read()
         if len(data) > 0:
             data = json.loads(data)
-        if 'result' not in data or 'status' not in data['result'] or data['result']['status'] != 'Ok':
+
+        if 'result' not in data or 'status' not in data['result'] or data['result']['status'] not in ['OkAsync', 'Ok']:
             log_message('Chyba při volání '+ str(url))
             ws.close()
             return { 'err' : 'Chyba při volání API' }  
-        ws.close()
-        if (type(get_config_value('debug')) == int and get_config_value('debug') > 0) or get_config_value('debug') == '1' or get_config_value('debug') == 'true':
-            if type(get_config_value('debug')) == int and get_config_value('debug') > 1 and len(str(data)) > get_config_value('debug'):
-                log_message('Odpověď obdržena (' + str(len(str(data))) + ')')
+        if data['result']['status'] == 'OkAsync':
+            response = ws.recv()
+            if (type(get_config_value('debug')) == int and get_config_value('debug') > 0) or get_config_value('debug') == '1' or get_config_value('debug') == 'true':
+                if type(get_config_value('debug')) == int and get_config_value('debug') > 1 and len(str(response)) > get_config_value('debug'):
+                    log_message('Odpověď obdržena (' + str(len(str(response))) + ')')
+                else:
+                    log_message(str(response))        
+            if response and len(response) > 0:
+                data = json.loads(response)
+                if 'response' not in data or 'result' not in data['response'] or 'status' not in data['response']['result'] or data['response']['result']['status'] != 'Ok' or data['response']['context']['requestId'] != requestId:
+                    log_message('Chyba při volání '+ str(url))
+                    ws.close()
+                    return { 'err' : 'Chyba při volání API' }  
+                ws.close()
+                if 'data' in data['response']:
+                    return data['response']['data']
+                return []
             else:
-                log_message(str(data))        
-        if 'result' not in data or 'status' not in data['result'] or data['result']['status'] != 'Ok' or data['context']['requestId'] != requestId:
-            log_message('Chyba při volání '+ str(url))
-            return { 'err' : 'Chyba při volání API' }
-        else:
-            if 'data' in data:
-                return data['data']
-        return []
+                ws.close()
+                return []
+        elif data['result']['status'] == 'Ok':
+            ws.close()
+            if (type(get_config_value('debug')) == int and get_config_value('debug') > 0) or get_config_value('debug') == '1' or get_config_value('debug') == 'true':
+                if type(get_config_value('debug')) == int and get_config_value('debug') > 1 and len(str(data)) > get_config_value('debug'):
+                    log_message('Odpověď obdržena (' + str(len(str(data))) + ')')
+                else:
+                    log_message(str(data))        
+            if 'result' not in data or 'status' not in data['result'] or data['result']['status'] != 'Ok' or data['context']['requestId'] != requestId:
+                log_message('Chyba při volání '+ str(url))
+                return { 'err' : 'Chyba při volání API' }
+            else:
+                if 'data' in data:
+                    return data['data']
+            return []
     except HTTPError as e:
         log_message('Chyba při volání '+ str(url) + ': ' + e.reason)
         ws.close()
